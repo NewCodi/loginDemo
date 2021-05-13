cb40de09d27bc784edb99a02d98189e4fd812255
allenyau <newcodingage@gmail.com>
2021-05-13 11:34:51 +0800

    Initial Commit

---
+++ loginDemo/loginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginDemoApp.swift
+//  loginDemo
+//
+//  Created by YAUTAKWO on 13/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct loginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
